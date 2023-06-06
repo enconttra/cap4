0855768c8409f68056b05f297c9d1572591bdaad
enconttra <135740937+enconttra@users.noreply.github.com>
2023-06-06 15:31:58 -0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1DZIWMEDfBKC0riC7tVTzM0Pp_vKKr69UCEICoatNtbQ/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = 'AIzaSyA1lgIMxDOCqoTiggOOFE5ex6ZibU1_zz4';
