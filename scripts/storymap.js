2c71e713483d2c48efe0342e83f43f7dffa2adb7
enconttra <135740937+enconttra@users.noreply.github.com>
2023-10-02 15:46:14 -0300

    Update storymap.js

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -407,7 +407,24 @@ $(window).on('load', function() {
         // 
           if (c['GeoJSON Overlay 2']) {            
             $.getJSON(c['GeoJSON Overlay 2'], function(geojson) {
-
+              if (c['Marker Image']) {
+                function createCustomIcon (feature, latlng) {
+                  let myIcon = L.icon({
+                    iconUrl: c['Marker Image'],
+                    //shadowUrl: c['Marker Image'],
+                    iconSize:     [25, 25], // width and height of the image in pixels
+                    //shadowSize:   [35, 20], // width, height of optional shadow image
+                    iconAnchor:   [12, 12] //, // point of the icon which will correspond to marker's location
+                    //shadowAnchor: [12, 6],  // anchor point of the shadow. should be offset
+                    //popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+                  })
+                  return L.marker(latlng, { icon: myIcon })
+                }
+                let myLayerOptions = {
+                  pointToLayer: createCustomIcon
+                }
+                geoJsonOverlay2 = L.geoJson(geojson, myLayerOptions).addTo(map);
+              } else {
               // Parse properties string into a JS object
               var props = {};
 
